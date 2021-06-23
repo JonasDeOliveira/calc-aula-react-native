6413d921095dba36138d85a523c2e3ecc21a9406
Jonas Oliveira <jloide.essence@rd.com.br>
2021-06-23 10:37:18 -0300

    button component

--- src/screens/Calculator.js
+++ src/screens/Calculator.js
@@ -3,6 +3,7 @@ import {
     StyleSheet, View
  } from 'react-native'
  import Display from '../components/Display'
+ import Button from '../components/Button'
 
  export default class Calculator extends Component {
 
@@ -10,13 +11,38 @@ import {
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Display/>
+                <View style={styles.buttons}>
+                    <Button label='AC' threeCollumns/>
+                    <Button label='/' operation/>
+                    <Button label='7'/>
+                    <Button label='8'/>
+                    <Button label='9'/>
+                    <Button label='*' operation/>
+                    <Button label='4'/>
+                    <Button label='5'/>
+                    <Button label='6'/>
+                    <Button label='-' operation/>
+                    <Button label='1'/>
+                    <Button label='2'/>
+                    <Button label='3'/>
+                    <Button label='+' operation/>
+                    <Button label='0' twoCollumns/>
+                    <Button label='.'/>
+                    <Button label='=' operation/>
+                </View>
             </View>
         )
     }
  }
 
 const styles = StyleSheet.create({
-     
+    container: {
+        flex: 1
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
 })
